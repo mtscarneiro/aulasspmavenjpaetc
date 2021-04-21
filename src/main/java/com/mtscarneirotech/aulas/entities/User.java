7f3bfcea56af5c2aac31f3aaf9267b83b651dac0
Matheus Carneiro <mchcar@vivaldi.net>
2021-04-21 19:34:14 -0400

    H2 database, test profile, JPA

--- src/main/java/com/mtscarneirotech/aulas/entities/User.java
+++ src/main/java/com/mtscarneirotech/aulas/entities/User.java
@@ -2,17 +2,26 @@ package com.mtscarneirotech.aulas.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	public User() {}
+
+	public User() {
+	}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -87,6 +96,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-
 
 }
