4ad37dd92436b8e1a74111fc903f4f8539fe2f51
mtscarneiro <mchcar@vivaldi.net>
2021-04-23 11:51:33 -0400

    Product>>OrderItem, @OneToMany

--- src/main/java/com/mtscarneirotech/application/entities/pk/OrderItemPK.java
+++ src/main/java/com/mtscarneirotech/application/entities/pk/OrderItemPK.java
@@ -6,6 +6,7 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mtscarneirotech.application.entities.Order;
 import com.mtscarneirotech.application.entities.Product;
 
