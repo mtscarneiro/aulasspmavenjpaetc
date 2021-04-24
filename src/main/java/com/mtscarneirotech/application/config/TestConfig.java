1fde9bbdb7c2965dcba6f15ddcd8adb49277769c
mtscarneiro <mchcar@vivaldi.net>
2021-04-24 09:55:46 -0400

    Payment>> OneToOne <<Order

--- src/main/java/com/mtscarneirotech/application/config/TestConfig.java
+++ src/main/java/com/mtscarneirotech/application/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.mtscarneirotech.application.entities.Category;
 import com.mtscarneirotech.application.entities.Order;
 import com.mtscarneirotech.application.entities.OrderItem;
+import com.mtscarneirotech.application.entities.Payment;
 import com.mtscarneirotech.application.entities.Product;
 import com.mtscarneirotech.application.entities.User;
 import com.mtscarneirotech.application.entities.enums.OrderStatus;
@@ -77,6 +78,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 
 }
